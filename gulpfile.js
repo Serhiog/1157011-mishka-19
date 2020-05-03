99bf80cd97604402634a4bfb65cba6eb1adb5835
Сергей Грищенков <grishenkov.sergei@gmail.com>
2020-05-03 03:01:33 -0500

    Проверка автоматизации

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,10 @@ var server = require("browser-sync").create();
 var rename = require("gulp-rename");
 var imagemin = require("gulp-imagemin");
 var webp = require("gulp-webp");
-
+var sprite = require("gulp-svgstore");
+var posthtml = require("gulp-posthtml");
+var include = require("posthtml-include");
+var del = require("del")
 
 gulp.task("css", function () {
   return gulp.src("source/less/style.less")
@@ -24,13 +27,14 @@ gulp.task("css", function () {
     .pipe(csso())
     .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
+
     .pipe(server.stream());
 });
 
 gulp.task("server", function () {
   server.init({
-    server: "source/",
+    server: "build/",
     notify: false,
     open: true,
     cors: true,
@@ -41,21 +45,56 @@ gulp.task("server", function () {
   gulp.watch("source/*.html").on("change", server.reload);
 });
 
-gulp.task("images", function() {
-  return gulp.src("source/img/**/*.{png,jpg,svg}")
+gulp.task("images", function () {
+  return gulp.src("source/img/*.{png,jpg}")
     .pipe(imagemin([
       imagemin.optipng({optimizationLevel: 5}),
       imagemin.mozjpeg({progressive: true}),
       imagemin.svgo()
     ]))
-    .pipe(gulp.dest("source/img"));
+    .pipe(gulp.dest("build/img"));
 });
 
-gulp.task ("webp", function () {
-  return gulp.src ("source/img/**/*.{jpg, png}")
-    .pipe (webp())
-    .pipe(gulp.dest("source/img"));
+gulp.task("webp", function () {
+  return gulp.src("source/img/**/*.{jpg, png}")
+    .pipe(webp())
+    .pipe(gulp.dest("build/img"));
 });
 
+gulp.task("sprite", function () {
+  return gulp
+    .src("source/img/icon-*.svg")
+    .pipe(sprite({
+      inlineSvg: true
+    }))
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("build/img/"));
+});
 
-gulp.task("start", gulp.series("css", "server"));
+gulp.task("posthtml", function () {
+  return gulp
+    .src("source/*.html")
+    .pipe(posthtml([
+      include()
+    ]))
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("copy", function () {
+  return gulp.src([
+    "source/fonts/**/8.{woff, woff2}",
+    "source/img/**",
+    "source/js/**",
+    "source/*ico"
+  ], {
+    base: "source"
+  })
+    .pipe(gulp.dest("build"));
+})
+
+gulp.task("clean", function () {
+  return del("build");
+})
+
+gulp.task("build", gulp.series("clear", "copy", "css", "images", "webp", "posthtml"));
+gulp.task("start", gulp.series("build", "server"));
